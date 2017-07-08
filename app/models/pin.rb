94c5b7030c2a6e5ffc65d28430c33c32a0b4d650
superman7445 <Slastbench02@gmail.com>
2017-07-08 13:28:21 +0530

    initial commit

---
+++ app/models/pin.rb
@@ -0,0 +1,10 @@
+class Pin < ActiveRecord::Base
+	belongs_to :user
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+
+	validates :image, presence: true
+	validates :description, presence: true
+end
